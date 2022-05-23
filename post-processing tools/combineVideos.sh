1dcd4fcee73bf3a6033eb635dda13acd739eb44c
Markus Poeschl <Poeschl@users.noreply.github.com>
2022-05-23 18:24:06 +0100

    :lipstick: Add timelapse and full hd scale to video transcode script

--- post-processing tools/combineVideos.sh
+++ post-processing tools/combineVideos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This little tool combine recorded pixelflut .mks into a big standardized h265 video mp4.
+# This little tool combine recorded pixelflut .mks into a big standardized h265 video mp4 with a 4 times speed up time modifier.
 # This is normally used to spread the recording on your favorite video sharing platform.
 #
 set -e
@@ -11,6 +11,8 @@ RESULT_FILE='combined.mp4'
 if [ -z "$INPUT_FILES" ]; then
   echo 'No input files are detected.'
   echo 'Arguments like this are expected: ./combineVideos.sh PixelTest-2022-04-30_10-26.mkv PixelTest-2022-04-30_10-30.mkv'
+  echo 'For a full folder of files use ./combineVideos.sh $(ls | sort)'
+  exit 1
 fi
 
 echo "Start creating video of files $@"
@@ -27,8 +29,10 @@ done
 set -x
 ffmpeg \
   $INPUTS \
-  -filter_complex "$INPUT_VIDEO_STREAMS concat=n=${INPUT_COUNT}:v=1:a=0 [combinedv]" \
-  -map '[combinedv]' -vcodec hevc_nvenc -preset fast -b:v 6M -an \
+  -filter_complex "$INPUT_VIDEO_STREAMS concat=n=${INPUT_COUNT}:v=1:a=0 [combinedv]; \
+  [combinedv] setpts=0.25*PTS [timelapse]; \
+  [timelapse] scale=w=1920:h=1080:force_original_aspect_ratio=decrease [scaled]" \
+  -map '[scaled]' -vcodec hevc_nvenc -preset fast -b:v 6M -an \
   -r 30 ${RESULT_FILE}
 set +x
 
